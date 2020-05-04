f6259658df16e012bf00b0138324fe8fbf2119aa
yannrichet <richetyann@gmail.com>
2020-05-04 20:15:34 +0200

    add CRISTAL V2.0.3

--- src/main/scripts/Cristal-SnKeff_2.0.3.sh
+++ src/main/scripts/Cristal-SnKeff_2.0.3.sh
@@ -2,4 +2,4 @@
 
 export CRISTAL_HOME="/opt/CRISTAL_V2"
 
-$CRISTAL_HOME/scripts.v2.0/cristal.v2.0 $*
+$CRISTAL_HOME/scripts.v2.0/cristal.v2.0.3 $*
--- src/main/scripts/Cristal-SnNormes_2.0.sh
+++ src/main/scripts/Cristal-SnNormes_2.0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export CRISTAL_HOME="/produits/sec/FORMULAIRE_CRISTAL_V2"
+export CRISTAL_HOME="/opt/CRISTAL_V2"
 
 $CRISTAL_HOME/scripts.v2.0/cristal.v2.0 $*
